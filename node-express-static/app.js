3be96090ce4a3fed218da7a6c121f9035a72f64f
yee2542 <yee2542gtl@gmail.com>
2019-03-29 22:07:10 +0700

    add simple express static

---
+++ node-express-static/app.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.listen(8080, () => {
+  console.log('server express start at 8080')
+})
