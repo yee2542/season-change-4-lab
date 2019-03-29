c4b270b42bed19ac6a44828d941f4efb581003cf
yee2542 <yee2542gtl@gmail.com>
2019-03-30 00:54:08 +0700

    add simple session authen

---
+++ node-simple-session/app.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const express = require('express')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+
+const authen = require('./authen')
+
+const app = express()
+
+app.use(session({
+  secret: 'secretja',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/login.html'))
+})
+
+app.post('/login', (req, res) => {
+  authen(req.body.username, req.body.password, (state) => {
+    if (state) {
+      req.session.username = state
+    } else {
+    }
+  })
+  res.redirect('/login/callback')
+})
+
+app.get('/login/callback', (req, res) => {
+  if (req.session) {
+    if (req.session.username) {
+      res.redirect('/user')
+    }
+  } else {
+    res.redirect('/login')
+  }
+})
+
+app.get('/user', (req, res) => {
+  if (req.session) {
+    if (req.session.username) {
+      res.send(req.session.username)
+    }
+  }
+  res.redirect('/login')
+})
+
+app.listen(8080, () => {
+  console.log('start server simple session at 8080')
+})
