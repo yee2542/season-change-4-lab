e88f132c93c451de5019ad102497967c8077f7e3
yee2542 <yee2542gtl@gmail.com>
2019-03-30 01:20:33 +0700

    unused morgan

--- node-simple-session/app.js
+++ node-simple-session/app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
+// const morgan = require('morgan')
 
 const authen = require('./authen')
 
@@ -13,6 +14,7 @@ app.use(session({
   saveUninitialized: false
 }))
 
+// app.use(morgan())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
