2535b4c5a02beb8c3110dd456c655c07d67fb10f
yee2542 <yee2542gtl@gmail.com>
2019-03-30 07:14:17 +0700

    preparing simple session cluster

---
+++ node-simple-session-cluster/app.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const express = require('express')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+// const morgan = require('morgan')
+
+const authen = require('./authen')
+
+const app = express()
+
+app.use(session({
+  secret: 'secretja',
+  resave: false,
+  saveUninitialized: false
+}))
+
+// app.use(morgan())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.static('public'))
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/login.html'))
+})
+
+app.post('/login', (req, res) => {
+  authen(req.body.username, req.body.password, (state) => {
+    if (state) {
+      req.session.username = state
+    } else {
+    }
+  })
+  res.redirect('/login/callback')
+})
+
+app.get('/login/callback', (req, res) => {
+  if (req.session) {
+    if (req.session.username) {
+      res.redirect('/user')
+    }
+  } else {
+    res.redirect('/login')
+  }
+})
+
+app.get('/user', (req, res) => {
+  if (req.session) {
+    if (req.session.username) {
+      res.send(req.session.username)
+    }
+  }
+  res.redirect('/login')
+})
+
+const cluster = require('cluster')
+const numCPUs = 3
+const http = require('http')
+const server = http.createServer(app)
+
+require('sticky-cluster')(
+
+  // server initialization function
+  callback => {
+    // configure an app
+    // do some async stuff if needed
+
+    // don't do server.listen(), just pass the server instance into the callback
+    callback(server)
+  },
+
+  // options
+  {
+    concurrency: 4,
+    port: 8081,
+    // debug: true
+  }
+)
